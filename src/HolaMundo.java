41b6950582dfb7d11a42189aee8a18a2b4f52ea1
amejor <sarpera2c@gmail.com>
2015-03-20 11:24:05 +0000

    cambio en web

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 
-		System.out.print("Hola mundo Cruel Ruben");
+		System.out.print("Hola mundo Cruel Ruben, hoy es viernes");
 	}
 
 }
